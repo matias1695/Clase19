8ac71f2fcf1d108adae76d40a7a796745e9c64ce
Matias Velez <velez.matias95@gmail.com>
2024-07-18 19:09:32 -0300

    appcoder_1.1

--- appcoder/views.py
+++ appcoder/views.py
@@ -3,23 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-""""
-def crear_curso(request):
-    nombre=input("Ingrese el nombre del curso: ")
-    comision=input("Ingrese el numero de comicion")
-    curso=Curso(nombre=nombre,comision=comision)
-    curso.save()
-    text= f"Curso Creado y registrado"
-    return HttpResponse(text)
-
-def mostrar_cursos(request):
-    cursos=Curso.objects.all()
-    
-    contexto={"Cursos":cursos}
-    
-    return render(request,"cursos.html",contexto)
-    
-"""
 def inicio(request):
     return render(request,"appcoder/inicio.html")
 
